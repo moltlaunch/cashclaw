89ced0ca3adc0ce7603542baa36356c7c84b81a6
LaphoqueRC <91871936+LaphoqueRC@users.noreply.github.com>
2026-03-21 06:59:11 +0300

    fix(core): add vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,15 @@ export default defineConfig({
   build: {
     outDir: "../../dist/ui",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    },
+    assetsDir: 'assets',
+    manifest: true
   },
   server: {
     proxy: {
